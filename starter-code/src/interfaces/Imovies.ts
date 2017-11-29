b5eb25cd207c05f79fbefb338874b09f45164483
Dario Vigil <dariovigil@gmail.com>
2017-11-29 21:23:10 +0100

    First iteration complete.

---
+++ starter-code/src/interfaces/Imovies.ts
@@ -0,0 +1,12 @@
+export interface IMovies {
+     id: number,
+     title: string,
+     poster: string,
+     synopsis: string,
+     genres: Array<string>,
+     year: number,
+     director: string,
+     actors: Array<string>,
+     hours: Array<string>,
+     room: number,
+}
